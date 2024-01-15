851000971b597b70e9fcc33abf7961c9f31bd219
abhay-ag <abhay0034.be21@chitkara.edu.in>
2024-01-15 10:47:23 +0530

    Added admin view && tables

--- app/components/user-nav.tsx
+++ app/components/user-nav.tsx
@@ -9,22 +9,24 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
 
-export function UserNav() {
+export function UserNav({ admin }: { admin?: boolean }) {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
           <Avatar className="h-10 w-10">
-            <AvatarFallback>AA</AvatarFallback>
+            <AvatarFallback>{admin ? "A" : "AA"}</AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56" align="end" forceMount>
         <DropdownMenuLabel className="font-normal">
           <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">Abhay Aggarwal</p>
+            <p className="text-sm font-medium leading-none">
+              {admin ? "Admin" : "Abhay Aggarwal"}
+            </p>
             <p className="text-xs leading-none text-muted-foreground">
-              2110990034
+              {admin ? "admin" : "2110990034"}
             </p>
           </div>
         </DropdownMenuLabel>
