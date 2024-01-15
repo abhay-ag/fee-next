2e8f65338a98aa6dd80ee2364922c83eff5e98ed
AbhayG21 <abhay19021@gmail.com>
2024-01-15 09:47:17 +0530

    Centered the login form

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import { UserAuthForm } from "./components/UserAuthForm";
 export default function AuthenticationPage() {
   return (
     <>
-      <div className="container relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
+      <div className="container h-screen flex items-center justify-center lg:max-w-none align-middle lg:px-0">
+        {/* <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
           <div className="absolute inset-0 bg-zinc-900" />
           <div className="relative z-20 flex items-center text-lg font-medium">
             <svg
@@ -31,7 +31,7 @@ export default function AuthenticationPage() {
               <footer className="text-sm">Abhay Aggarwal</footer>
             </blockquote>
           </div>
-        </div>
+        </div> */}
         <div className="lg:p-8">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
