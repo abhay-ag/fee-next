0c1c4206b699e9a27a223336a2330fa5fa415693
abhay-ag <abhay0034.be21@chitkara.edu.in>
2024-01-15 10:47:23 +0530

    Added admin view && tables

---
+++ app/admin/page.tsx
@@ -0,0 +1,33 @@
+import { Metadata } from "next";
+import { Overview } from "@/app/components/overview";
+import { UserNav } from "@/app/components/user-nav";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { DataTableDemo } from "../components/data-table";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+  description: "Student Dashboard",
+};
+
+export default function DashboardPage() {
+  return (
+    <>
+      <div className="min-h-screen space-y-4 p-8 pt-6">
+        <div className="flex items-center justify-between space-y-2">
+          <h2 className="text-3xl font-bold tracking-tight">DashLMS</h2>
+          <div className="flex gap-3 items-center">
+            {new Date().toLocaleDateString(undefined, {
+              weekday: "long",
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            })}
+            <UserNav admin />
+          </div>
+        </div>
+        <DataTableDemo />
+      </div>
+    </>
+  );
+}
