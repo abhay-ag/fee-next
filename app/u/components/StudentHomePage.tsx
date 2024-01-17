a72d25752bff8288bb8a196579ef62163993c06f
abhay-ag <abhay0034.be21@chitkara.edu.in>
2024-01-17 12:57:26 +0530

    more security added

--- app/u/components/StudentHomePage.tsx
+++ app/u/components/StudentHomePage.tsx
@@ -4,13 +4,15 @@ import { userDetails } from "@/app/states/userDetails";
 import { userState } from "@/app/states/userState";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { LoadingOutlined } from "@ant-design/icons";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 
 export default function StudentComponent() {
   const [user, setUser] = useRecoilState(userState);
-  const [userData, setUserData] = useRecoilState(userDetails);
+  const [userData, setUserData] = useRecoilState<any>(userDetails);
+  const [loading, setLoading] = useState(true);
   const router = useRouter();
 
   async function getStudentData() {
@@ -23,6 +25,11 @@ export default function StudentComponent() {
       body: JSON.stringify({ roll_no: user }),
     }).then(async (resp) => {
       const data = await resp.json();
+      if (data.state === "Not Found") {
+        setUser("");
+        localStorage.setItem("roll_no", "");
+        router.push("/");
+      }
       setUserData(data.data);
     });
   }
@@ -30,40 +37,52 @@ export default function StudentComponent() {
   useEffect(() => {
     if (localStorage.getItem("roll_no") !== "") {
       setUser(localStorage.getItem("roll_no") as string);
-      getStudentData();
     } else {
       router.push("/");
+    }
+  }, []);
+
+  useEffect(() => {
+    if (user.length) {
+      getStudentData();
+      setLoading(false);
     }
   }, [user]);
   return (
     <>
-      <Tabs defaultValue="overview" className="space-y-4">
-        <TabsList>
-          <TabsTrigger value="overview">Overview</TabsTrigger>
-          <TabsTrigger value="analytics">Course Content</TabsTrigger>
-          <TabsTrigger value="reports">Performance</TabsTrigger>
-        </TabsList>
-        <TabsContent value="overview" className="space-y-4">
-          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-            <Card>
-              {/* <CardHeader> */}
+      {loading ? (
+        <div className="flex items-center justify-center w-screen h-screen">
+          <LoadingOutlined className="text-7xl" />
+        </div>
+      ) : (
+        <Tabs defaultValue="overview" className="space-y-4">
+          <TabsList>
+            <TabsTrigger value="overview">Overview</TabsTrigger>
+            <TabsTrigger value="analytics">Course Content</TabsTrigger>
+            <TabsTrigger value="reports">Performance</TabsTrigger>
+          </TabsList>
+          <TabsContent value="overview" className="space-y-4">
+            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+              <Card>
+                {/* <CardHeader> */}
                 <CardTitle>ASDF</CardTitle>
-              {/* </CardHeader> */}
-              {/* <CardContent></CardContent> */}
-            </Card>
-          </div>
-          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
-            <Card className="col-span-7">
-              <CardHeader>
-                <CardTitle>Overview</CardTitle>
-              </CardHeader>
-              <CardContent className="pl-2">
-                <Overview />
-              </CardContent>
-            </Card>
-          </div>
-        </TabsContent>
-      </Tabs>
+                {/* </CardHeader> */}
+                {/* <CardContent></CardContent> */}
+              </Card>
+            </div>
+            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
+              <Card className="col-span-7">
+                <CardHeader>
+                  <CardTitle>Overview</CardTitle>
+                </CardHeader>
+                <CardContent className="pl-2">
+                  <Overview />
+                </CardContent>
+              </Card>
+            </div>
+          </TabsContent>
+        </Tabs>
+      )}
     </>
   );
 }
