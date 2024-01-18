2e0d41449146db7560d5d8db073e2bb0e59475c3
abhay-ag <abhay0034.be21@chitkara.edu.in>
2024-01-18 11:13:29 +0530

    backend hosted || url changed

--- next.config.js
+++ next.config.js
@@ -6,55 +6,55 @@ const nextConfig = {
     return [
       {
         source: "/all",
-        destination: "http://localhost:8080/",
+        destination: "http://18.181.215.18:8080/",
       },
       {
         source: "/student/add",
-        destination: "http://localhost:8080/student/add",
+        destination: "http://18.181.215.18:8080/student/add",
       },
       {
         source: "/student/update",
-        destination: "http://localhost:8080/student/update",
+        destination: "http://18.181.215.18:8080/student/update",
       },
       {
         source: "/student/delete",
-        destination: "http://localhost:8080/student/delete",
+        destination: "http://18.181.215.18:8080/student/delete",
       },
       {
         source: "/user/login",
-        destination: "http://localhost:8080/login",
+        destination: "http://18.181.215.18:8080/login",
       },
       {
         source: "/student/get",
-        destination: "http://localhost:8080/student/get",
+        destination: "http://18.181.215.18:8080/student/get",
       },
       {
         source: "/courses/add",
-        destination: "http://localhost:8080/courses/add",
+        destination: "http://18.181.215.18:8080/courses/add",
       },
       {
         source: "/courses",
-        destination: "http://localhost:8080/courses",
+        destination: "http://18.181.215.18:8080/courses",
       },
       {
         source: "/courses/get",
-        destination: "http://localhost:8080/courses/get",
+        destination: "http://18.181.215.18:8080/courses/get",
       },
       {
         source: "/courses/byid",
-        destination: "http://localhost:8080/courses/get/by-id",
+        destination: "http://18.181.215.18:8080/courses/get/by-id",
       },
       {
         source: "/attendance/save",
-        destination: "http://localhost:8080/attendance/save",
+        destination: "http://18.181.215.18:8080/attendance/save",
       },
       {
         source: "/attendance/get",
-        destination: "http://localhost:8080/attendance/get",
+        destination: "http://18.181.215.18:8080/attendance/get",
       },
       {
         source: "/notification",
-        destination: "http://localhost:8080/notification",
+        destination: "http://18.181.215.18:8080/notification",
       },
     ];
   },
